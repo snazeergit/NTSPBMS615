61f5a79c12a93c70b06d9a88a16709af76c14c2c
Nazeer <nazeersd.java@gmail.com>
2022-06-17 22:06:28 +0530

    Rest Component updated

--- SpringRestProj08-SendingDataAsPathVariables/src/main/java/com/nt/rest/EmployeeOperationsManager.java
+++ SpringRestProj08-SendingDataAsPathVariables/src/main/java/com/nt/rest/EmployeeOperationsManager.java
@@ -6,15 +6,46 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.jsf.FacesContextUtils;
 
 @RestController
 @RequestMapping("/emp/api")
 public class EmployeeOperationsManager {
-	
-	@GetMapping("/show/{eno}/{ename}")//eno and ename are the path variables 
-	public ResponseEntity<String> showEmployeeDetails(@PathVariable("eno") Integer no, @PathVariable String ename) {
-		System.out.println(no + "<-------->" + ename);
-		return new ResponseEntity<String>(no + "<---->" + ename, HttpStatus.OK);
+
+	// executes only if the URL
+	// ......../show/<any integer>/<anything>
+	@GetMapping("/show/{eno}/{ename}") // eno and ename are the dynamic path variables
+	public ResponseEntity<String> showEmployeeDetails(@PathVariable(name = "eno", required = false) Integer no,
+			@PathVariable(required = false) String ename) {
+		System.out.println("EmployeeOperationsManager.showEmployeeDetails()");
+		return new ResponseEntity<String>("method with path : /show/{eno}/{ename}", HttpStatus.OK);
+	}
+
+	// executes only if the URL
+	// ......../show/eno/<anything>
+	@GetMapping("/show/eno/{ename}") // eno is static path variable and ename is dynamic path variable
+	public ResponseEntity<String> showEmployeeDetails1(@PathVariable(name = "eno", required = false) Integer no,
+			@PathVariable String ename) {
+		System.out.println("EmployeeOperationsManager.showEmployeeDetails1()");
+		return new ResponseEntity<String>("method with path : /show/eno/{ename}", HttpStatus.OK);
+	}
+
+	// executes only if the URL
+	// ......../show/<any number>/ename
+	@GetMapping("/show/{eno}/ename") // eno is dynamic path variable and ename is static path variable
+	public ResponseEntity<String> showEmployeeDetails2(@PathVariable(name = "eno", required = false) Integer no,
+			@PathVariable(required = false) String ename) {
+		System.out.println("EmployeeOperationsManager.showEmployeeDetails2()");
+		return new ResponseEntity<String>("method with path : /show/{eno}/ename", HttpStatus.OK);
+	}
+
+	// executes only if the URL
+	// ......../show/eno/ename
+	@GetMapping("/show/eno/ename") // eno and ename are static path variables
+	public ResponseEntity<String> showEmployeeDetails3(@PathVariable(name = "eno", required = false) Integer no,
+			@PathVariable(required = false) String ename) {
+		System.out.println("EmployeeOperationsManager.showEmployeeDetails3()");
+		return new ResponseEntity<String>("method with path : /show/eno/ename", HttpStatus.OK);
 	}
 
 }
