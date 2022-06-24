57b3d0b68bd7905919ecad88d9be878e079dd655
Nazeer <nazeersd.java@gmail.com>
2022-06-25 00:42:52 +0530

    All CURD operations included

--- SpringRestProj09-MiniProjectAsLayeredApp/src/main/java/com/nt/rest/ActorOperationsController.java
+++ SpringRestProj09-MiniProjectAsLayeredApp/src/main/java/com/nt/rest/ActorOperationsController.java
@@ -1,11 +1,16 @@
 package com.nt.rest;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -60,6 +65,57 @@ public class ActorOperationsController {
 			//use any of getAllActorsInSorting1() or getAllActorsInSorting2() or getAllActorsInSorting3()
 			Iterable<Actor> iterable = service.getAllActorsInSorting3();
 			return new ResponseEntity<Iterable<Actor>>(iterable, HttpStatus.OK);
+		} catch (Exception e) {
+			e.printStackTrace();
+			return new ResponseEntity<String>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+	}
+
+	@GetMapping("/getActor/{category1}/{category2}")
+	public ResponseEntity<?> fetchActorsByCategories(@PathVariable String category1, @PathVariable String category2) {
+		try {
+			List<Actor> actorsList = service.fetchActorsByCategories(category1, category2);
+			return new ResponseEntity<List<Actor>>(actorsList, HttpStatus.OK);
+
+		} catch (Exception e) {
+			e.printStackTrace();
+			return new ResponseEntity<String>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+	}
+
+	@PutMapping("/updateActor")
+	public ResponseEntity<?> updateActor(@RequestBody Actor actor) {
+
+		try {
+			String msg = service.updateActor(actor);
+			return new ResponseEntity<String>(msg, HttpStatus.ACCEPTED);
+
+		} catch (Exception e) {
+			e.printStackTrace();
+			return new ResponseEntity<String>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+	}
+
+	@DeleteMapping("/deleteActor/{id}")
+	public ResponseEntity<?> deleteActor(@PathVariable Integer id) {
+
+		try {
+			String msg = service.deleteActor(id);
+			return new ResponseEntity<String>(msg, HttpStatus.ACCEPTED);
+
+		} catch (Exception e) {
+			e.printStackTrace();
+			return new ResponseEntity<String>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+	}
+
+	@PatchMapping("/updatemobileNo/{id}/{mobile}")
+	public ResponseEntity<?> updateActorMobileNo(@PathVariable Integer id, @PathVariable("mobile") long newMobileNo) {
+
+		try {
+			String msg = service.updateActorMobileNo(id, newMobileNo);
+			return new ResponseEntity<String>(msg, HttpStatus.ACCEPTED);
+
 		} catch (Exception e) {
 			e.printStackTrace();
 			return new ResponseEntity<String>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
--- SpringRestProj09-MiniProjectAsLayeredApp/src/main/java/com/nt/service/ActorMgmtServiceImpl.java
+++ SpringRestProj09-MiniProjectAsLayeredApp/src/main/java/com/nt/service/ActorMgmtServiceImpl.java
@@ -2,13 +2,16 @@ package com.nt.service;
 
 import java.util.Collections;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collector;
 import java.util.stream.Collectors;
 import java.util.stream.StreamSupport;
 
+import org.aspectj.apache.bcel.classfile.Module.Open;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.nt.exception.ActorNotFoundException;
 import com.nt.model.Actor;
 import com.nt.repository.IActorRepo;
 
@@ -58,6 +61,48 @@ public class ActorMgmtServiceImpl implements IActorMgmtService {
 		List<Actor> list = StreamSupport.stream(iterable.spliterator(), false)
 				.sorted((t1, t2) -> t1.getActorName().compareTo(t2.getActorName())).collect(Collectors.toList());
 		return list;
+	}
+
+	@Override
+	public List<Actor> fetchActorsByCategories(String category1, String category2) {
+		List<Actor> actorsList = actorRepo.getActorsByCategories(category1, category2);
+		return actorsList;
+	}
+
+	@Override
+	public String updateActor(Actor actor) {
+		Integer actorId = actor.getActorId();
+		Optional<Actor> optional = actorRepo.findById(actorId);
+		if (optional.isPresent()) {
+			actorRepo.save(actor);
+			return "Actor record updated";
+		} else {
+			throw new ActorNotFoundException("No actor is available with id " + actorId + " provided");
+		}
+	}
+
+	@Override
+	public String deleteActor(Integer id) {
+		Optional<Actor> optional = actorRepo.findById(id);
+		if (optional.isPresent()) {
+			Actor actor = optional.get();
+			actorRepo.delete(actor);
+			return "Actor with id " + id + " is deleted";
+		} else {
+			throw new ActorNotFoundException("No actor is available with id " + id + " provided");
+		}
+	}
+
+	@Override
+	public String updateActorMobileNo(Integer id,long newMobileNo) {
+		Optional<Actor> optional = actorRepo.findById(id);
+		if (optional.isPresent()) {
+			Actor actor = optional.get();
+			actor.setMobileNo(newMobileNo);
+			return "Actor's mobile number is updated";
+		} else {
+			throw new ActorNotFoundException("No actor is available with id " + id + " provided");
+		}
 	}
 
 }
