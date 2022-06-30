10d8b083417c3f9d1e07d4498639f7c07b451c88
Nazeer <nazeersd.java@gmail.com>
2022-07-01 01:52:32 +0530

    Consumer web application to test REST Provider Comp

---
+++ SpringRestProj12-2-ConsumerWebApp/src/main/java/com/nt/runner/WishMessageConsumerRunner.java
@@ -0,0 +1,29 @@
+package com.nt.runner;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.autoconfigure.template.TemplateAvailabilityProviders;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+
+@Component
+public class WishMessageConsumerRunner implements CommandLineRunner {
+
+	@Override
+	public void run(String... args) throws Exception {
+		//Create RestTemplate object
+		RestTemplate template =new RestTemplate();
+		//Prepare service/base url
+		String serviceUrl="http://localhost:2020/SpringRestProj12-1-ProviderApp/wish/api/message";
+//consume the service using xxxFOrEntity(-) method
+		ResponseEntity<String> response = template.getForEntity(serviceUrl, String.class);
+		//process response
+		System.out.println("Response Body: "+response.getBody());
+		System.out.println("Response Code: "+response.getStatusCode());
+		System.out.println("Response Headers: "+response.getHeaders());
+	}
+
+}
+
+//Runners in Spring boot application executes only when we run the application as the "Spring Boot App" or the "Java Application".
+//If run the application as "Run on Server" then Runners will not be executed so you will get output.
