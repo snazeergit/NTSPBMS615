64ed06f75c8a43d5052a2b36b053fe016ab842a9
Nazeer <nazeersd.java@gmail.com>
2022-07-30 00:30:25 +0530

    Config Server with custom port number using bootstrap.properties

---
+++ MSProj08-EurekaClient-ConfigClient-1/src/main/java/com/nt/rest/EmpOpsController.java
@@ -0,0 +1,24 @@
+package com.nt.rest;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/employee")
+public class EmpOpsController {
+
+	@Value("${dbuser}")
+	private String dbuser;
+
+	@Value("${dbpwd}")
+	private String dbpwd;
+
+	@GetMapping("/show")
+	public ResponseEntity<String> printDBCreds() {
+		return new ResponseEntity<String>("Emp--> DB Details:" + dbuser + "/" + dbpwd, HttpStatus.OK);
+	}
+}
---
+++ MSProj08-EurekaClient-ConfigClient-2/src/main/java/com/nt/MsProj08EurekaClientConfigClient2Application.java
@@ -0,0 +1,13 @@
+package com.nt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsProj08EurekaClientConfigClient2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsProj08EurekaClientConfigClient2Application.class, args);
+	}
+
+}
