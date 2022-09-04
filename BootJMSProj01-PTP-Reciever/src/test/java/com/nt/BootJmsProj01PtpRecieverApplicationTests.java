9cf4bd52f42a3cc73dce6fe267694a1805a4698e
Nazeer <nazeersd.java@gmail.com>
2022-09-05 01:24:45 +0530

    PTP Sender Receiver

---
+++ BootJMSProj01-PTP-Reciever/src/test/java/com/nt/BootJmsProj01PtpRecieverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootJmsProj01PtpRecieverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ BootJMSProj01-PTP-Sender/src/main/java/com/nt/BootJmsProj01PtpSenderApplication.java
@@ -0,0 +1,13 @@
+package com.nt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootJmsProj01PtpSenderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootJmsProj01PtpSenderApplication.class, args);
+	}
+
+}
